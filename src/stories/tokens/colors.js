e9e82e574d44afe32bdb956d38ccd1ed4a29060b
Diego Tejedor <diego.tejedor@grupor5.com>
2022-02-18 03:40:33 -0500

    feat: add button r5

---
+++ src/stories/tokens/colors.js
@@ -0,0 +1,32 @@
+const white = '#FFFFFF'
+const black = '#000000'
+
+
+const primary = {
+    dark: 'hsl(163, 100%, 38%)',
+    normal: '#00EBAA',
+    light: '#51F6B8',
+    superLight: '#94FBD6',
+}
+
+const secondary = {
+    dark: 'hsl(183, 100%, 39%)',
+    normal: '#67E0E6',
+    light: '#92E8EA',
+    superLight: '#BEF3F3',
+}
+
+const grays = {
+    gunmental: '#273140',
+    slateGray: '#717785',
+    silverSand: '#C3C7CE',
+    cadetBlueCrayola: '#A4ACB8',
+  }
+
+export const colors = {
+    white,
+    black,
+    primary,
+    secondary,
+    grays
+}
